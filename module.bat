dccfc4f2af679f91b363b355d1156b74bd37afbb
F4LL <61364610+1337F4LL1337@users.noreply.github.com>
2020-03-24 13:34:26 +0100

    Add files via upload

---
+++ module.bat
@@ -0,0 +1,7 @@
+@echo off
+title Installation des modules...
+color a
+echo Installation des modules
+color c 
+npm i selfbot.js
+pause
---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+title Rakuzan V2
+echo.
+echo.
+color a
+echo                                                 ================
+echo                                                 =  Rakuzan V2  =
+echo                                                 ================
+echo.
+timeout /t 4 /nobreak >nul
+cls
+node index.js
+pause
